8e2915d8cb97d6ce1f828ac756fe3b1d7c83cc89
unwriter <unwriter@protonmail.com>
2019-08-14 08:28:32 -0400

    comment

--- planaria/index.js
+++ planaria/index.js
@@ -33,20 +33,33 @@ const start = async function(p) {
     console.log("PLANARIA", errors.join("\n"));
     process.exit(1);
   } else {
+    /*
+    * cases:
+    * 1. default bus path + default tape path: (!p.tape && !p.src) => use current path for p.tape, initialize bus at current path.
+    * 2. custom bus path + default tape path: (!p.tape && p.src) => use current path for p.tape, use the bus path 
+    * 3. custom bus path + custom tape path: (p.tape && p.src) => use the custom path for p.tape, use the bus path
+    * 4. default bus path + custom tape path: (p.tape && !p.src) => use the custom path for p.tape, initialize bus at p.tape
+    */
     let buspath;
     if (p.tape) {
+      // if 'tape' exists, use that as the tape path
       p.tape = path.resolve(".", p.tape)
     } else {
+      // if 'tape' doesn't exist, use current path as tape path
       p.tape = process.cwd()
     }
     if (!p.src) {
+      // if 'src' doesn't exist,
+      // initialize bitbus with tape path
       await bitbus.init({ BUS_PATH: p.tape })
       buspath = await bitbus.build(p.filter)    
+      // set 'src' with the initialized buspath and 'filter.from'
       p.src = {
         from: p.filter.from,
         path: buspath
       }
     } else {
+      // if 'src' exists, assume that bitbus is already initialized, and use 'src.path' as buspath
       buspath = p.src.path;
     }
     let current = await tape.current(buspath, p)
